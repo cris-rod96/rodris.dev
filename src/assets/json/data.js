af98030c632b54d68c1a0f528bdf088a73e3d78c
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-04-23 01:55:03 -0500

    Header Section

--- src/assets/json/data.js
+++ src/assets/json/data.js
@@ -5,6 +5,7 @@ import {
   RiMailLine,
   RiUserLine,
 } from "react-icons/ri";
+import { SiGithub, SiLinkedin } from "react-icons/si";
 
 export const menu = [
   {
@@ -30,6 +31,24 @@ export const menu = [
   {
     path: "/contact",
     label: "Contact",
+    icon: RiMailLine,
+  },
+];
+
+export const social = [
+  {
+    link: "https://github.com/cris-rod96",
+    label: "Github",
+    icon: SiGithub,
+  },
+  {
+    link: "https://linkedin.com/in/cristhian-rodriguez-659779205/",
+    label: "LinkedIn",
+    icon: SiLinkedin,
+  },
+  {
+    link: "mailto:crisrodam1996@gmail.com?subject=Quiero contactar contigo",
+    label: "Correo electrónico",
     icon: RiMailLine,
   },
 ];
